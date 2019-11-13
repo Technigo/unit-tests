a8b44d6ef451c3aca9b5a4fc28053f89637fb812
Frida Jonsson <annafridamariajonsson@gmail.com>
2019-11-13 10:54:52 +0100

    Test 1 & 2 done

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,13 @@
 export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
+  if (items.length >= 2 ) {
+  // should return the first and last items from an array with two items
+  // should return the first and last items from an array with many item
+    return `First: ${items[0]}, Last: ${items[items.length-1]}`
+  } else if (items.length == 1) {
+    // Should only return the first item if the array only has one item
+    return `Only item: ${items[0]}`
+  } else {
+    // Should return "No items!" if the array is empty'
+    return "No items!"
+  }
 }
