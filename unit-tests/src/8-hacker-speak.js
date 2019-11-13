bbc7ed6a691a55a41436d61040dfe378812cda46
Charlotte Holm <50885736+HannaCharlotteHolm@users.noreply.github.com>
2019-11-13 21:34:32 +0100

    8-hacker-speak

--- unit-tests/src/8-hacker-speak.js
+++ unit-tests/src/8-hacker-speak.js
@@ -1,3 +1,12 @@
 export const hackerSpeak = (words) => {
-  return words
+  
+  var lettersToNumbers = words
+  .replace(/a/gi,"4")
+  .replace(/i/gi,"1")
+  .replace(/e/gi,"3")
+  .replace(/o/gi,"0")
+  .replace(/s/gi,"5")
+
+  return lettersToNumbers
+
 }
