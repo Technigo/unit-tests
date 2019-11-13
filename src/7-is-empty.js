f9ac2f0088a0685fe2e128e5f2fbf261d3a1e757
Emmi Nilsson <emmie.nilsson@gmail.com>
2019-11-13 19:56:51 +0100

    Did all unit tests.

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,13 @@
+import { objectExpression } from "@babel/types"
+
 export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
 
   if (type === 'string') {
     return stringArrayOrObject === ''
+  } else if (type === 'array') {
+    return stringArrayOrObject.length === 0
+  } else if (type === 'object') {
+    return Object.entries(stringArrayOrObject).length === 0
   }
-
-  return false
 }
