7f6e72b27a28c2afddb86a7115b00bfa5a77f50b
Jens_Nilsen <jensnilsen89@gmail.com>
2019-11-13 21:42:44 +0100

    finished unit-test. need to clean out comments and notes

--- src/3-find-index.js
+++ src/3-find-index.js
@@ -1,3 +1,6 @@
 export const findIndex = (array, value) => {
-  return
-}
+  return array.indexOf(value);
+};
+
+// how to get index of item in array javascript
+//  array.indexOf(value)
--- src/7-is-empty.spec.js
+++ src/7-is-empty.spec.js
@@ -1,27 +1,27 @@
-import { isEmpty } from './7-is-empty'
+import { isEmpty } from "./7-is-empty";
 
-describe('7-is-empty', () => {
-  it('should return true for an empty string', () => {
-    expect(isEmpty('')).toEqual(true)
-  })
+describe("7-is-empty", () => {
+  it("should return true for an empty string", () => {
+    expect(isEmpty("")).toEqual(true);
+  });
 
-  it('should return false for a string with a vaue', () => {
-    expect(isEmpty('hello')).toEqual(false)
-  })
+  it("should return false for a string with a value", () => {
+    expect(isEmpty("hello")).toEqual(false);
+  });
 
-  it('should return true for an empty array', () => {
-    expect(isEmpty([])).toEqual(true)
-  })
+  it("should return true for an empty array", () => {
+    expect(isEmpty([])).toEqual(true);
+  });
 
-  it('should return false for an array with items', () => {
-    expect(isEmpty(['hello', 'world'])).toEqual(false)
-  })
+  it("should return false for an array with items", () => {
+    expect(isEmpty(["hello", "world"])).toEqual(false);
+  });
 
-  it('should return true for an empty object', () => {
-    expect(isEmpty({})).toEqual(true)
-  })
+  it("should return true for an empty object", () => {
+    expect(isEmpty({})).toEqual(true);
+  });
 
-  it('should return false for an object with properties', () => {
-    expect(isEmpty({ name: 'Jane' })).toEqual(false)
-  })
-})
+  it("should return false for an object with properties", () => {
+    expect(isEmpty({ name: "Jane" })).toEqual(false);
+  });
+});
