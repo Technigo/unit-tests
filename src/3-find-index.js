4da5279e2d0a4d6f52fd52c9846162838da1f52f
Frida Jonsson <annafridamariajonsson@gmail.com>
2019-11-13 12:55:16 +0100

    Test 4 done

--- src/3-find-index.js
+++ src/3-find-index.js
@@ -1,3 +1,3 @@
-export const findIndex = (array, value) => {
-  return
+export const showIndex = (array) => {
+  return (array.index)
 }
--- src/3-find-index.spec.js
+++ src/3-find-index.spec.js
@@ -4,18 +4,18 @@ describe('3-find-index', () => {
   const items = ['Apple', 'Banana', 'Orange', 'Pear', 'Lemon']
 
   it('should return the correct index of the first item', () => {
-    expect(findIndex(items, 'Apple')).toEqual(0)
+    expect(showIndex(items, 'Apple')).toEqual(0)
   })
 
   it('should return the correct index of the last item', () => {
-    expect(findIndex(items, 'Lemon')).toEqual(4)
+    expect(showIndex(items, 'Lemon')).toEqual(4)
   })
 
   it('should return the correct index of an item in the middle', () => {
-    expect(findIndex(items, 'Orange')).toEqual(2)
+    expect(showIndex(items, 'Orange')).toEqual(2)
   })
 
   it('should return -1 if the item is not in the array', () => {
-    expect(findIndex(items, 'Pineapple')).toEqual(-1)
+    expect(showIndex(items, 'Pineapple')).toEqual(-1)
   })
 })
