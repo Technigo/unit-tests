8f6ee18fab815755ae13f9686a3d59fe83bb19bd
Frida Jonsson <annafridamariajonsson@gmail.com>
2019-11-16 17:53:37 +0100

    Test 8 done (but probably could have been done better)

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,11 @@
 export const hackerSpeak = (words) => {
-  return words
+  
+  // words.replace(/ 4/g, "A");
+  // var allAs = /A/gi;
+  // var allIs = /I/gi;
+
+  return words.replace(/A/g,'4').replace(/I/g, '1').replace(/S/g, '5').replace(/E/g, '3').replace(/O/g, '0').replace(/a/g,'4').replace(/i/g, '1').replace(/s/g, '5').replace(/o/g, '0').replace(/e/g, '3')
+
+
 }
+
