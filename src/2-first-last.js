b776b8305c2bece6660c728cc11b382c7145f9db
codebyfredrik <50440682+codebyfredrik@users.noreply.github.com>
2019-11-17 12:08:49 +0100

    Updating unit tests

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,15 @@
-export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
-}
+export const firstLast = items => {
+  if (items.length === 0) {
+    return 'No items!';
+  }
+
+  if (items.length === 1) {
+    return `Only item: ${items}`;
+  }
+
+  if (items.length > 2) {
+    return `First: ${items[0]}, Last: ${items[items.length - 1]}`;
+  }
+
+  return `First: ${items[0]}, Last: ${items[1]}`;
+};
