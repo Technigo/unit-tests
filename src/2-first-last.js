2a33533932975cc453d99ff8cbb931fd138343cd
Petra B <petrabrinck@icloud.com>
2019-11-19 23:03:23 +0100

    5 of 9 done

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,12 @@
 export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
+  if (items.length === 0) {
+    return `No items!`
+
+  }
+  if (items.length === 1) {
+    return `Only item: ${items[0]}`
+  }
+  {
+    return `First: ${items[0]}, last: ${items[items.length - 1]}`
+  }
 }
