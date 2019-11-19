890e18ff68e3694fe516dca01a0b1de8408e1c97
Ivett Backlund <ivett.backlund@gmail.com>
2019-11-19 22:01:20 +0100

    unit tests 1 to 6

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -4,5 +4,10 @@ const months = [
 ]
 
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
+  if (monthNumber < 1 || monthNumber > 12) {
+    return null
+  }
+  else {
+    return months[monthNumber - 1]
+  }
 }
