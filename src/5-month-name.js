5a98eff668398c6cea24b8407aebd671a03733a4
Christina Persson <christina@Christinas-MacBook.local>
2020-03-01 16:56:46 +0100

    Improved one solution

--- src/5-month-name.js
+++ src/5-month-name.js
@@ -14,13 +14,9 @@ const months = [
 ];
 
 export const monthName = monthNumber => {
-  if (monthNumber === 1) {
-    return months[monthNumber - 1];
-  } else if (monthNumber === 6) {
-    return months[monthNumber - 1];
-  } else if (monthNumber === 12) {
-    return months[monthNumber - 1];
-  } else if (monthNumber < 1 || monthNumber > 12) {
+  if (monthNumber < 1 || monthNumber > 12) {
     return null;
+  } else {
+    return months[monthNumber - 1];
   }
 };
