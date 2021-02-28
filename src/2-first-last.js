afff695a46d81e7b747f3c3f85cabf59f6e72b5e
Frivridna <54737644+Frivridna@users.noreply.github.com>
2021-02-28 21:34:24 +0100

    fix the test first-last

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,12 +1,9 @@
 export const firstLast = (items) => {
-  if (items.length === 2) {
-    return `First: ${items[0]}, Last: ${items[1]}`
+  if (items.length > 1) {
+    return `First: ${items[0]}, Last: ${items[items.length -1]}` 
   }
-  else if (items.length >= 3) { 
-    return `First: ${items[0]}, Last: ${items[4]}`
-  } 
   else if (items.length === 1) {
-    return `Only item: bob`
+    return `Only item: ${items[0]}`
   }
   else if (items.length === 0) {
     return `No items!`
