8ac12591b0f0888434e5cab8302693ae992faf46
Frivridna <54737644+Frivridna@users.noreply.github.com>
2021-02-28 21:23:12 +0100

    fix code for tests

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,14 @@
 export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
+  if (items.length === 2) {
+    return `First: ${items[0]}, Last: ${items[1]}`
+  }
+  else if (items.length >= 3) { 
+    return `First: ${items[0]}, Last: ${items[4]}`
+  } 
+  else if (items.length === 1) {
+    return `Only item: bob`
+  }
+  else if (items.length === 0) {
+    return `No items!`
+  }
 }
--- src/9-hashtags.js
+++ src/9-hashtags.js
@@ -1,3 +1,11 @@
 export const hashtags = (text) => {
-  return text
+  const regex = /#\S+/g
+  const texten = text.match(regex)
+  return texten
 }
+//regex that first found only the #
+//searched and found match() method - retrieves the result of matching a string against a regular expression
+//tried to understand g = globally in the entire string, i = ignore case, had to look up Regular expression cheatsheet https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Cheatsheet
+//continuation of regular expression S = Matches a single character other than white space. 
+//contintuation of regular expression + = matches minimum 1 character \S in this case, but took the entire word. Found solution on Stack overflow.
+//continuation of regular expression * = is "greedy" / is a bit more generous --> in this case it would be minimum 0 characters. 
