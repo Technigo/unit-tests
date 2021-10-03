60f69a86ee4457d9bf5c0217360de94ef1743c58
Jenny Quach <quach.jen@gmail.com>
2021-10-03 23:24:55 +0200

    resolved unit tests

--- src/6-filter-numbers.js
+++ src/6-filter-numbers.js
@@ -1,3 +1,9 @@
 export const filterNumbers = (array, largerThan) => {
   return array
 }
+
+return array.filter(num => num <= largerThan)
+
+//array is [1,20,25,34,99]
+//largerThan is 25
+//should return [1,20,25]
--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -4,6 +4,13 @@ export const isEmpty = (stringArrayOrObject) => {
   if (type === 'string') {
     return stringArrayOrObject === ''
   }
+if (Array.isArray(stringArrayOrObject)) {
+  return stringArrayOrObject.length === 0
+}
+
+if (type === 'object') {
+  return Object.entries(stringArrayOrObject).length === 0
+}
 
   return false
 }
