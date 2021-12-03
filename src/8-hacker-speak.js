c8b44acc8b3eb0f625a9594587536979410ef7ce
Moa Blomkvist <moa@birgerochblom.se>
2021-12-03 14:04:56 +0100

    test 8

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,5 +1,13 @@
 export const hackerSpeak = (words) => {
   
+  let newCharacter = words.replace(/a|A/g,'4').replace(/e|E/g,'3').replace(/i|I/g,'1').replace(/o|O/g,'0').replace(/s|S/g,'5')
+  newCharacter.toUpperCase()
 
-  return words
-}
+  return newCharacter
+
+};
+
+
+
+
+
