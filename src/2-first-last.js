7b1c34ebf95f0976203db895da67ef7a57fee363
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-02-22 23:04:27 +0100

    5 out of 9 exercises completed

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,11 @@
 export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
-}
+  if (items.length === 0) {
+    return "No items!";
+  } else if (items.length === 1) {
+    return `Only item: ${items[0]}`;
+  } else if (items.length > 2) {
+    return `First: ${items[0]}, Last: ${items[items.length - 1]}`;
+  } else {
+    return `First: ${items[0]}, Last: ${items[1]}`;
+  }
+};
