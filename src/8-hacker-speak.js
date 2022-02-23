9fae54d854e5844120cb3899400ff865ab0a3861
Emma Högberg <emmahogberg88@gmail.com>
2022-02-23 11:14:36 +0100

    cleaner solution for 8-hacker-speak

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,14 +1,9 @@
 export const hackerSpeak = (words) => {
 
   return words
-  .replace(/a/g, '4')
-  .replace(/e/g, '3')
-  .replace(/i/g, '1')
-  .replace(/o/g, '0')
-  .replace(/s/g, '5')
-  .replace(/A/g, '4')
-  .replace(/E/g, '3')
-  .replace(/I/g, '1')
-  .replace(/O/g, '0')
-  .replace(/S/g, '5');
+  .replace(/a/gi, '4')
+  .replace(/e/gi, '3')
+  .replace(/i/gi, '1')
+  .replace(/o/gi, '0')
+  .replace(/s/gi, '5')
 }
