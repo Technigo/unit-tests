1221061b8ba8e5c1ab9269d6c299ef891bf59714
joannaringqvist <joanna.ringqvist@gmail.com>
2022-02-27 15:31:03 +0100

    Unit tests week 7

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,13 @@
-export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
+export const firstLast = (items, value) => {
+
+  const arrayLength = items.length;
+
+  if (arrayLength === 1) {
+    return `Only item: ${items[0]}`;
+  } else if (!arrayLength) {
+    return `No items!`;
+  } else {
+    return `First: ${items[0]}, Last: ${items[arrayLength-1]}`
+  }
+
 }
--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -2,8 +2,12 @@ export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
 
   if (type === 'string') {
-    return stringArrayOrObject === ''
+    return stringArrayOrObject === '';
   }
 
+  if (type === 'object') {
+    return Object.entries(stringArrayOrObject).length === 0;
+  }
+  
   return false
 }
