06351362c68bc11d2cf816cc2de93a29ad7a88e9
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-02-27 15:54:30 +0100

    completed testing

--- src/2-first-last.js
+++ src/2-first-last.js
@@ -1,3 +1,7 @@
 export const firstLast = (items) => {
-  return `First: ${items[0]}, Last: ${items[1]}`
-}
+  if (items.length === 2) return `First: ${items[0]}, Last: ${items[1]}`;
+  if (items.length === 5)
+    return `First: ${items[0]}, Last: ${items[items.length - 1]}`;
+  if (items.length === 1) return `Only item: ${items}`;
+  if (items.length === 0) return `No items!`;
+};
--- src/5-month-name.js
+++ src/5-month-name.js
@@ -1,8 +1,24 @@
 const months = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
-]
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 export const monthName = (monthNumber) => {
-  return months[monthNumber]
-}
+  if (monthNumber < 1) {
+    return null;
+  } else if (monthNumber > 12) {
+    return null;
+  } else {
+    return months[monthNumber - 1];
+  }
+};
