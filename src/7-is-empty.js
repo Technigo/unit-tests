3ec1ab49f6805dcd473219686ff59bb4cc8aebe3
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-02-27 23:55:33 +0100

    8 out of 9

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,13 @@
 export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
-
-  if (type === 'string') {
-    return stringArrayOrObject === ''
+  if (type === "string") {
+    return stringArrayOrObject === "";
+  }
+  if (type === "array") {
+    return stringArrayOrObject.length === 0;
+  }
+  if (type === "object") {
+    return Object.keys(stringArrayOrObject).length === 0;
   }
 
-  return false
-}
+  return false;
+};
